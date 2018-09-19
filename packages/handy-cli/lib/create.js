940afd04f05ae8b98b7b92eb9681e06d43deb771
xiyuyizhi <1553810509@qq.com>
2018-09-19 23:01:44 +0800

    (feat): add handy-service

--- packages/handy-cli/lib/create.js
+++ packages/handy-cli/lib/create.js
@@ -22,20 +22,26 @@ async function creator(appName) {
     }
   }
   fs.mkdirSync(appDir);
-  const confirm = new Confirm(appName, getModuleList(path.resolve(__dirname, "../", "prompts")));
+  const confirm = new Confirm(
+    appName,
+    getModuleList(path.resolve(__dirname, "../", "prompts"))
+  );
   const answers = await confirm.confirm();
   const pkg = {
     name: appName,
     version: "0.0.1",
     devDependencies: {},
-    script: {
+    scripts: {
       start: "handy-service serve",
       build: "handy-service build"
     }
   };
   pkg.preset = answers;
   const package = new Package();
-  package.write(path.resolve(appDir, "package.json"), JSON.stringify(pkg, null, 2));
+  package.write(
+    path.resolve(appDir, "package.json"),
+    JSON.stringify(pkg, null, 2)
+  );
   console.log(answers);
 }
 
